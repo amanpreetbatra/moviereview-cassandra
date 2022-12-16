3caf28b76c479f5f1dcded2466d450a85004211e
Rajat Sharma <32574865+rajatjc@users.noreply.github.com>
2022-12-16 03:12:53 -0500

    ikp udpate

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import sync_table
 # from app import IP, KEYSPACE
 KEYSPACE = "movie_keyspace"
-IP = '127.0.0.1'
+IP = '52.188.66.157'
 connection.setup([IP], KEYSPACE, protocol_version=3)
 
 
